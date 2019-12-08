b464c3f19360c6944cff7a01cf53fffeb99c0f62
lelilia <lelischiestl@zedat.fu-berlin.de>
2019-12-07 22:49:07 -0500

    day 6 is done

--- 2019/day6-pt2.rb
+++ 2019/day6-pt2.rb
@@ -1,4 +1,4 @@
-#https://adventofcode.com/2019/day/6
+# https://adventofcode.com/2019/day/6#part2
 
 class Orbits
 	attr_accessor :orbits
@@ -15,6 +15,13 @@ class Orbits
 		return depth if a == b
 		depth = depth + 1
 		how_many_orbits(a, @orbits[b][0], depth) 
+	end
+
+	def which_orbits(a,b,path)
+		path = [b] + path 
+		return path if a == b
+		
+		which_orbits(a, @orbits[b][0], path)
 	end
 
 end
@@ -34,3 +41,16 @@ list_of_all_the_planets.each do |planet|
 	res += orbits_in_this_puzzle.how_many_orbits("COM",planet,0)
 end
 p res
+
+you =  orbits_in_this_puzzle.which_orbits("COM", "YOU", [])
+san =  orbits_in_this_puzzle.which_orbits("COM", "SAN", [])
+
+i = 0
+while true
+	if you[i] == san[i]
+		i += 1
+	else 
+		p you[i..].length + san[i..].length - 2
+		break
+	end
+end
